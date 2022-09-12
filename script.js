2d3e55a5a42780e9ea31b2371f1a4c5a301c4267
Adel Ashraf <adelashrafhamouda@gmail.com>
2022-09-12 20:16:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 const renderCountry = function (data, className) {
